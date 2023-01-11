8858ac50285b907e07e0510e7d432516e09cda6f
Михаил Семенович <semenovich_misha@mail.ru>
2023-01-11 20:37:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Myname;
+            Myname = "Misha";
+            Console.WriteLine(Myname);
+            Console.ReadKey 
+        }
+    }
+}
